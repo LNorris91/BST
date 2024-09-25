6a5b4b461e3618115f215e2730fbb838dd4ab346
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-09-25 15:02:20 -0400

    Commit Creates Project and Adds Starter Functions
    this commit creates the project and creates a Node
    class and a Tree class with methods in the Tree
    class to create the tree, insert new values into
    the tree and also delete items from the tree using
    recursive functions.

---
+++ main.js
@@ -0,0 +1,23 @@
+import Tree from './tree.js';
+const newArray = [1, 4, 43, 12, 55, 78, 55, 10];
+const maple = new Tree(newArray);
+
+function prettyPrint(node, prefix = '', isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
+
+maple.insert(33);
+maple.insert(9);
+maple.insert(8);
+maple.insert(7);
+maple.deleteItem(4);
+prettyPrint(maple.root);
---
+++ tree.js
@@ -0,0 +1,89 @@
+import Node from './node.js';
+
+export default class Tree {
+  constructor(array) {
+    this.array = this.refineArray(array);
+    this.root = this.buildTree(this.array);
+  }
+
+  refineArray(array) {
+    let refinedArray = [];
+    let sortedArray = array.sort(function (a, b) {
+      return a - b;
+    });
+    sortedArray.forEach((element) => {
+      if (!refinedArray.includes(element)) {
+        refinedArray.push(element);
+      }
+    });
+
+    console.log(refinedArray);
+    return refinedArray;
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const mid = Math.floor(array.length / 2);
+    const node = new Node(array[mid]);
+
+    node.left = this.buildTree(array.slice(0, mid));
+    node.right = this.buildTree(array.slice(mid + 1));
+    console.log(node);
+    return node;
+  }
+
+  insert(value) {
+    this.findInsert(this.root, value);
+  }
+
+  findInsert(root, value) {
+    if (root === null) {
+      return new Node(value);
+    }
+
+    if (root.data === value) {
+      return root;
+    }
+
+    if (value < root.data) {
+      root.left = this.findInsert(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.findInsert(root.right, value);
+    }
+
+    return root;
+  }
+
+  findSuccessor(current) {
+    current = current.right;
+    while (current !== null && current.left !== null) {
+      current = current.left;
+    }
+    return current;
+  }
+
+  deleteItem(value) {
+    this.findDeleteItem(this.root, value);
+  }
+
+  findDeleteItem(root, value) {
+    if (root === null) {
+      return root;
+    }
+
+    if (value < root.data) {
+      root.left = this.findDeleteItem(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.findDeleteItem(root.right, value);
+    } else {
+      if (root.left === null) return root.right;
+      if (root.right === null) return root.left;
+
+      let succ = this.findSuccessor(root);
+      root.data = succ.data;
+      root.right = this.findDeleteItem(root.right, succ.data);
+    }
+    return root;
+  }
+}
